1a00086b5ce10119ae76a8abfedad9196aad59da
tangdy <tdy853839625@qq.com>
2022-03-02 23:29:40 +0800

    fix:table展示的input先改成内容

--- src/page/create/FormTable.vue
+++ src/page/create/FormTable.vue
@@ -14,6 +14,7 @@
         border
         fit
         highlight-current-row
+        @current-change="changeCurrent"
         style="width: 100%"
       >
         <!-- <el-table-column type="selection" width="55" /> -->
@@ -35,14 +36,22 @@
               placeholder="搜索"
             />
           </template>
-          <template #default="{ row }">
-            <el-input v-model="row.text" size="small" placeholder="搜索" />
+          <template #default="{ row, $index }">
+            <span v-if="$index != currentItemIndex">{{ row.text }}</span>
+            <el-input
+              v-else
+              v-model="row.text"
+              size="small"
+              placeholder="搜索"
+            />
           </template>
         </el-table-column>
 
         <el-table-column width="125px" align="center" label="权重">
-          <template #default="{ row }">
+          <template #default="{ row, $index }">
+            <span v-if="$index != currentItemIndex">{{ row.size }}</span>
             <el-input-number
+              v-else
               style="width: 85px"
               v-model="row.size"
               :min="1"
@@ -54,8 +63,12 @@
         </el-table-column>
 
         <el-table-column width="105px" align="center" label="旋转">
-          <template #default="{ row }">
+          <template #default="{ row, $index }">
+            <span v-if="$index != currentItemIndex">{{
+              row.rotate === undefined ? '默认随机生成' : row.rotate
+            }}</span>
             <el-input
+              v-else
               style="width: 80px"
               v-model="row.rotate"
               placeholder="默认随机生成"
@@ -65,9 +78,10 @@
         </el-table-column>
 
         <el-table-column width="90px" label="颜色">
-          <template #default="{ row }">
-            <span v-if="!row.color">随机</span>
+          <template #default="{ row, $index }">
+            <div v-if="$index != currentItemIndex && !row.color">随机</div>
             <el-color-picker
+              v-if="$index == currentItemIndex"
               @change="(val) => updateObject(val, 'color', row)"
               v-model="row.color"
               :predefine="predefineColors"
@@ -76,8 +90,9 @@
         </el-table-column>
 
         <el-table-column width="120px" label="字体">
-          <template #default="{ row }">
-            <el-select v-model="row.fontFamily" placeholder="选择字体">
+          <template #default="{ row, $index }">
+            <span v-if="$index != currentItemIndex">{{ row.fontFamily }}</span>
+            <el-select v-else v-model="row.fontFamily" placeholder="选择字体">
               <el-option label="随机" value=""></el-option>
               <el-option label="Zone one" value="shanghai"></el-option>
               <el-option label="Zone two" value="beijing"></el-option>
@@ -127,19 +142,26 @@ export default {
       '#c71585',
     ]
     return {
+      currentItemIndex: NaN,
       Delete: DeleteIcon,
       list: [
         { text: '螺蛳粉', size: 40, color: '', idKey: getUuiD() },
         { text: '重庆小面', size: 35, color: '', idKey: getUuiD() },
         { text: '肉夹馍', size: 35, color: '', idKey: getUuiD() },
         { text: '炸酱面', size: 32, color: '', idKey: getUuiD() },
-        // { text: '沙县小吃', size: 25, color: '' },
-        // { text: '烤冷面', size: 23, color: '' },
-        // { text: '臭豆腐', size: 23, color: '' },
-        // { text: '钵钵鸡', size: 20, color: '' },
-        // { text: '酸辣粉', size: 19, color: '' },
+        { text: '沙县小吃', size: 25, color: '', idKey: getUuiD() },
+        { text: '烤冷面', size: 23, color: '', idKey: getUuiD() },
+        { text: '臭豆腐', size: 23, color: '', idKey: getUuiD() },
+        { text: '钵钵鸡', size: 20, color: '', idKey: getUuiD() },
+        { text: '酸辣粉', size: 19, color: '', idKey: getUuiD() },
         { text: '冒菜', size: 15, color: '', idKey: getUuiD() },
         { text: '驴打滚', size: 12, color: '', idKey: getUuiD() },
+        ...new Array(10).fill(1).map((v, i) => ({
+          text: `驴${i}`,
+          size: 12 + ~~(Math.random() * 10),
+          color: '',
+          idKey: getUuiD(),
+        })),
       ],
       predefineColors,
       dynamicValidateForm: {
@@ -147,17 +169,32 @@ export default {
         new: '',
         list: [],
       },
-      listLoading: true,
+      listLoading: false,
     }
   },
   created() {
-    this.getList()
+    // this.getList()
+    this.dynamicValidateForm.list = this.list
     this.addEventListren()
   },
   mounted() {
     this.emitterRender()
   },
   methods: {
+    changeCurrent(current) {
+      const index = this.dynamicValidateForm.list.findIndex(
+        (item) => current == item
+      )
+      this.currentItemIndex = index
+    },
+    getList() {
+      return new Array(20).fill(1).map((v, i) => ({
+        text: `驴打滚${i}`,
+        size: 12,
+        color: '',
+        idKey: getUuiD(),
+      }))
+    },
     updateObject(val, type, item) {
       emitter.emit('updateObject', {
         item,
@@ -188,11 +225,10 @@ export default {
     delteItem(index) {
       this.dynamicValidateForm.list.splice(index, 1)
     },
-    async getList() {
-      this.listLoading = true
-      this.dynamicValidateForm.list = this.list
-      this.listLoading = false
-    },
+    // async getList() {
+    //   this.listLoading = true
+    //   this.listLoading = false
+    // },
   },
 }
 </script>
